2435a471fba430e7665ae99091b9b05942e4788b
Checho0001 <sverbar@gmail.com>
2025-06-30 22:32:35 -0700

    added urls and updated settings and models

--- bitwise/models.py
+++ bitwise/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Bitwise(models.Model):
-    original = models.JSONField()
-    sorted_over_10 = models.JSONField()
+    original = models.TextField()
+    sorted_over_10 = models.TextField()
     average = models.FloatField()
     count_positive = models.IntegerField()
-    even_odd = models.JSONField()
-    warnings = models.JSONField()
+    even_odd = models.TextField()
+    warnings = models.TextField()
 
     def __str__(self):
         return f"Bitwise Result (Avg: {self.average}, Pos: {self.count_positive})"
